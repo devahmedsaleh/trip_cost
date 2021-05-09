8ebd0a0a7523946d669bb51132313f8068e5a6e7
Ahmed Saleh <engahmedsaleh@outlook.com>
2021-05-10 01:04:33 +0200

    add app layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(TripCostApp());
+
+class TripCostApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trip Cost Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Trip Cost',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
