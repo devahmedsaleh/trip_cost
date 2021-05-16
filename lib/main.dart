b13a717418b65556fe6d1de26e9e8b5986bd0d8f
Ahmed Saleh <engahmedsaleh@outlook.com>
2021-05-16 09:50:42 +0200

    add distance input field

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,41 @@ class TripCostApp extends StatelessWidget {
           title: Text('Trip Cost'),
           centerTitle: true,
         ),
+        body: TripForm(),
+      ),
+    );
+  }
+}
+
+class TripForm extends StatefulWidget {
+  @override
+  _TripFormState createState() => _TripFormState();
+}
+
+class _TripFormState extends State<TripForm> {
+  var distanceCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppTheme.padding),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: AppTheme.padding / 2),
+            child: TextField(
+              controller: distanceCtrl,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Distance',
+                hintText: 'e.g. 124',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
