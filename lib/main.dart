fc68b9c9e2ad9783364871f5335dd284ca45ca0b
Ahmed Saleh <engahmedsaleh@outlook.com>
2021-05-16 09:45:06 +0200

    add app theme class

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(TripCostApp());
 
+class AppTheme {
+  static const textScaleFactor = 1.5;
+  static const padding = 10.0;
+}
+
 class TripCostApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
